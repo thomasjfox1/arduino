e25799b05f4adb99f3bcca19cc8187059b1be54a
Thomas J. Fox <thomasjfox1@gmail.com>
2015-04-22 15:14:36 -0500

    added circuit diagram as well as additional elements and comments to stepper code

--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -1,3 +1,5 @@
+//This code is designed to control a stepper motor with a pontentiometer as speed control.
+
 #include <Stepper.h>
 
 const int stepsPerRevolution = 200;
@@ -13,7 +15,7 @@ const int brakeB = 8;
 const int directionA = 12;
 const int directionB = 13;
 
-const int potentiometer = 7;
+const int potentiometer = A1;
 
 int x = 0;
 void setup() {
@@ -40,6 +42,9 @@ Serial.begin(9600);
 void loop() {
   
   int potentioValue = analogRead(potentiometer); //potentiometer can read between 0 and 1023
+  
+  Serial.write("potentioValue: " + potentioValue); //debugging purposes
+  
   int calculatedSpeed = potentioValue/4;         //divided by 4 to limit rpms
   
   myStepper.setSpeed(calculatedSpeed);
