4907ae62bcad8779b1cfff69424deec4b91c10e2
Thomas J. Fox <thomasjfox1@gmail.com>
2015-04-25 21:31:46 -0500

    added comments and yo-yo sequence

--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -1,13 +1,16 @@
-//This code is designed to control a stepper motor with a pontentiometer as speed control.
-
 #include <Stepper.h>
+
+//ALL UNITS NEED TO BE IN CENTIMETERS
 
 const int stepsPerRevolution = 200;
 const float Pi = 3.141593;
-const int spoolDiameter = 10;
-float distanceCounter;
-float stepDegree = 360/stepsPerRevolution;
+const int spoolDiameter = 10; //10cm
+
+//distance counter to keep track of roughly where the object is in space 
+float distanceCounter = 0;  //starts at 0 meaning that's the location of the ball at the very beginning
+const float stepDegree = 360/stepsPerRevolution;
 int currentSpeed = 0;
+const float stringLength = 200; //200cm
 
 // initialize the stepper library on the motor shield
 Stepper myStepper(stepsPerRevolution, 12,13);     
@@ -20,15 +23,13 @@ const int brakeB = 8;
 const int directionA = 12;
 const int directionB = 13;
 
-int x = 0;
 void setup() {
   Serial.begin(9600);
-  // set the PWM and brake pins so that the direction pins  // can be used to control the motor:
+  // set the PWM and brake pins so that the direction pins  
   pinMode(pwmA, OUTPUT);
   pinMode(pwmB, OUTPUT);
   pinMode(brakeA, OUTPUT);
   pinMode(brakeB, OUTPUT);
-  
   digitalWrite(pwmA, HIGH);
   digitalWrite(pwmB, HIGH);
   digitalWrite(brakeA, LOW);
@@ -36,20 +37,34 @@ void setup() {
   
   // initialize the serial port:
   Serial.begin(9600);
-  // myStepper.setSpeed(30);
 }
 
 
 void loop() {
-  myStepper.setSpeed(currentSpeed);
-  currentSpeed = accelerateUp(myStepper, currentSpeed, 200, 1.2);
+  // this will cut the program if the object gets too close to the top or bottom
+  while(distanceCounter < stringLength){
+    myStepper.setSpeed(currentSpeed);
+    
+    currentSpeed = accelerateDown(myStepper, currentSpeed, 200, 1.2);
+    myStepper.setSpeed(currentSpeed);
+    myStepper.step(travelCalc(100));
+    distance(travelCalc(100));
+    currentSpeed = decelerateDown(myStepper, currentSpeed, 0, 0.8);
   
-  myStepper.setSpeed(200);
-  myStepper.step(200);
-  myStepper.step(-200);
-
+    delay(100);  
+    
+    currentSpeed = accelerateUp(myStepper, currentSpeed, 200, 1.2);
+    myStepper.setSpeed(currentSpeed);
+    myStepper.step(-travelCalc(100));
+    distance(-travelCalc(100));
+    currentSpeed = decelerateUp(myStepper, currentSpeed, 0, 0.8);
+    
+    delay(100);
+  }
 }
 
+
+//accelerate the object's speed downwards in an exponential manner
 int accelerateDown(Stepper stepper, int initialSpeed, int maxSpeed, int accelerationRate){
   int currentSpeed = initialSpeed;
   while(currentSpeed <= maxSpeed){
@@ -76,6 +91,7 @@ int accelerateDown(Stepper stepper, int initialSpeed, int maxSpeed, int accelera
   return currentSpeed;
 }
 
+//decelerate the object's speed downwards in an exponential manner
 int decelerateDown(Stepper stepper, int initialSpeed, int minSpeed, int decelerationRate){
   int currentSpeed = initialSpeed;
   while(currentSpeed >= minSpeed){
@@ -94,6 +110,7 @@ int decelerateDown(Stepper stepper, int initialSpeed, int minSpeed, int decelera
   return currentSpeed;
 }
 
+//accelerate the object's speed upwards in an exponential manner
 int accelerateUp(Stepper stepper, int initialSpeed, int maxSpeed, int accelerationRate){
   int currentSpeed = initialSpeed;
   while(currentSpeed <= maxSpeed){
@@ -121,6 +138,7 @@ int accelerateUp(Stepper stepper, int initialSpeed, int maxSpeed, int accelerati
   return currentSpeed;
 }
 
+//decelerate the object's speed upwards in an exponential manner
 int decelerateUp(Stepper stepper, int initialSpeed, int minSpeed, int decelerationRate){
   int currentSpeed = initialSpeed;
   while(currentSpeed >= minSpeed){
@@ -139,7 +157,14 @@ int decelerateUp(Stepper stepper, int initialSpeed, int minSpeed, int decelerati
   return currentSpeed;
 }
 
+//returns how many steps are needed to go x number of centimeters
+int travelCalc(float measurement){
+  float circMeasure = Pi * measurement;
+  return stepsPerRevolution * (circMeasure/(Pi*spoolDiameter));
+}
+
+//keep track of the distance the object is in the air so it does not go too high or too low
 float distance(int steps){
-  return distanceCounter + (steps * stepDegree);
+  return distanceCounter + (spoolDiameter * Pi) * (360 / (steps * stepDegree));
 }
 
